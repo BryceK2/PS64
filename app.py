f8be0d396d03caeef0576c269255f904a3cb18bf
Bryce Kelly <108435897+BryceK2@users.noreply.github.com>
2026-03-19 08:40:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,7 @@ def plot():
     )
     
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    # import os
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
